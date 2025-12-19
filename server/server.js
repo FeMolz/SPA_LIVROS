2fb96bd0aa7215d16c1d071c7b266ef64bec2ea1
Felipe Molz <felipeborbamolz@gmail.com>
2025-12-19 17:43:35 -0300

    Preparando para deploy

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cors from 'cors';
 import { connectDb } from './config/db.js';
 import bookRoutes from './routes/bookRoutes.js'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
-const app = express();  
-const PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000; 
 
 app.use(express.json());
 app.use(cors());
-app.use('/books', bookRoutes)
+
+app.use('/books', bookRoutes);
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 const startServer = async () => {
     await connectDb();
